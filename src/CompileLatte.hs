a4927ff8122963fcea568796588fc6ad80333c52
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2017-11-20 14:15:58 +0100

    Assignments.

--- src/CompileLatte.hs
+++ src/CompileLatte.hs
@@ -1,9 +1,11 @@
 module CompileLatte where
 
+import qualified Data.Map as M
 import Data.List (intercalate)
+import Data.Maybe (fromMaybe)
 import qualified AbsLatte
-import CompilerErr (CompilerErrorM)
-
+import CompilerErr (CompilerErrorM, raiseCEUndefinedVariable)
+import Control.Monad (foldM)
 
 latteMain = [ "target triple = \"x86_64-apple-macosx10.13.0\""
             , "@.str = private unnamed_addr constant [4 x i8] c\"%d\\0A\\00\", align 1"
@@ -25,6 +27,19 @@ latteMain = [ "target triple = \"x86_64-apple-macosx10.13.0\""
 
 newtype LLVMValue = LLVMConst Integer
 
+newtype ValueMap = ValueMap (M.Map String LLVMValue)
+
+setVariable :: String -> LLVMValue -> ValueMap -> ValueMap
+setVariable name value (ValueMap valueMap) =
+    ValueMap (M.insert name value valueMap)
+
+lookupVariable :: String -> ValueMap -> CompilerErrorM LLVMValue
+lookupVariable name (ValueMap valueMap) =
+    maybe (raiseCEUndefinedVariable name 0 0) return (M.lookup name valueMap)
+
+initValueMap :: ValueMap
+initValueMap = ValueMap M.empty
+
 compileLatte :: AbsLatte.Program -> CompilerErrorM String
 compileLatte (AbsLatte.Program topDefs) =
    do funcLines <- mapM compileFunc topDefs
@@ -33,28 +48,47 @@ compileLatte (AbsLatte.Program topDefs) =
 
 compileFunc :: AbsLatte.TopDef -> CompilerErrorM [String]
 compileFunc (AbsLatte.FnDef type_ ident args (AbsLatte.Block block)) =
-   do blockLines <- mapM compileStmt block
-      return $ ["define void @" ++ compileIdent ident ++ "() {"] ++ concat blockLines ++ ["ret void", "}"]
+   do (blockLines, _) <- foldM go ([], initValueMap) block
+      return $ ["define void @" ++ compileFuncIdent ident ++ "() {"] ++ blockLines ++ ["ret void", "}"]
 
-compileStmt :: AbsLatte.Stmt -> CompilerErrorM [String]
-compileStmt (AbsLatte.SExp expr) =
-   do (_, stmts) <- compileExpr expr
-      return stmts
+  where go (sLines, valueMap) statement =
+          do (newLines, newValueMap) <- compileStmt statement valueMap
+             return (sLines ++ newLines, newValueMap)
 
-compileExpr :: AbsLatte.Expr -> CompilerErrorM (LLVMValue, [String])
-compileExpr (AbsLatte.EApp ident args) =
-   do exprRes <- mapM compileExpr args
-      let statements = concatMap snd exprRes
-      let args = map fst exprRes
-      return (LLVMConst 0, statements ++ callInstr (compileIdent ident) args)
+compileStmt :: AbsLatte.Stmt -> ValueMap -> CompilerErrorM ([String], ValueMap)
+compileStmt (AbsLatte.SExp expr) valueMap =
+   do (_, stmts) <- compileExpr expr valueMap
+      return (stmts, valueMap)
+
+compileStmt (AbsLatte.Ass ident expr) valueMap =
+   do (value, stmts) <- compileExpr expr valueMap
+      let valueMap1 = setVariable (compileVariableIdent ident) value valueMap
+      return (stmts, valueMap1)
+
+compileStmt (AbsLatte.Decl _ [AbsLatte.Init ident expr]) valueMap =
+  compileStmt (AbsLatte.Ass ident expr) valueMap
+
+compileExpr :: AbsLatte.Expr -> ValueMap -> CompilerErrorM (LLVMValue, [String])
+compileExpr (AbsLatte.EApp ident args) valueMap =
+   do (sLines, argVals) <- foldM go ([], []) args
+      return (LLVMConst 0, sLines ++ callInstr (compileFuncIdent ident) argVals)
 
    where
+     go (sLines, argValues) argExpr =
+           do (val, newLines) <- compileExpr argExpr valueMap
+              return (sLines ++ newLines, argValues ++ [val])
      callInstr name args =
            ["call void @" ++ name ++ "(" ++ intercalate "," (map showArg args) ++  ")"]
      showArg (LLVMConst num) = "i32 " ++ show num
 
-compileExpr (AbsLatte.ELitInt int) =
+compileExpr (AbsLatte.ELitInt int) valueMap =
     return (LLVMConst int, [])
 
-compileIdent (AbsLatte.Ident str) | str == "printInt" = "printInt"
-                                  | otherwise = "lat_" ++ str
+compileExpr (AbsLatte.EVar ident) valueMap =
+  do value <- lookupVariable (compileVariableIdent ident) valueMap
+     return (value, [])
+
+compileFuncIdent (AbsLatte.Ident str) | str == "printInt" = "printInt"
+                                      | otherwise = "lat_" ++ str
+
+compileVariableIdent (AbsLatte.Ident str) = str
